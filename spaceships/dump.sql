82319629d5c7d1b5114f235d3147428aada3500e
RobertoDeLaRosaGonzalez <roberto.delarosa.gonzalez@gmail.com>
2024-02-28 21:05:17 +0100

    first commit

---
+++ spaceships/dump.sql
@@ -0,0 +1,21 @@
+-- --------------------------------------------------------
+-- Host:                         172.30.0.6
+-- Server Version:               8.0.23-0ubuntu0.20.04.1 - (Ubuntu)
+-- Server Betriebssystem:        Linux
+-- HeidiSQL Version:             10.3.0.5771
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Export Database structure for clearing_module
+CREATE DATABASE IF NOT EXISTS `clearing_module` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `clearing_module`;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
